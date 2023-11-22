aca39419280b85e6748978bd046985889fed6c80
BeatrizzJimenezz <cj21004@ues.edu.sv>
2023-11-22 17:56:18 -0600

    Dropdown, modalSesion

--- script/ModalLibros.js
+++ script/ModalLibros.js
@@ -52,3 +52,20 @@ function mostrarInformacionLibro(event) {
         `,
     });
 }
+
+function mostrarAlertaInicioSesion(event) {
+    event.preventDefault();
+
+    Swal.fire({
+        icon: "error",
+        iconColor: "#ED5584",
+        title: "Oops...",
+        footer: '<p class="">¿No tienes cuenta? <a href="../html/registro.html">Registrate aqui. </a></p>',
+        html: 
+        `
+        <p class="text-center">Inicia sesión para tener acceso a todas las opciones.</p>
+        `
+      });
+
+}
+
