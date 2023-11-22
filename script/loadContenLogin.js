c2013d6a99182e3749f96854a027e3ba14167f2f
Isaac684 <hc21018@ues.edu.sv>
2023-11-22 15:39:16 -0600

    Se agrego un scrip para usar un solo footer y nav

---
+++ script/loadContenLogin.js
@@ -0,0 +1,15 @@
+function includeContent(url, targetElement) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            targetElement.innerHTML += xhr.responseText;
+        }
+    };
+    xhr.send();
+}
+
+// Llamar a la función para incluir el footer cuando la página cargue
+window.onload = includeContent('../html/footer.html', document.getElementById('footer-container'));
+// Llamar a la función para incluir el encabezado cuando la página cargue
+window.onload = includeContent('../html/headerLogin.html', document.getElementById('header-container'));
