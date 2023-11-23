1b7bb932b7e75e1a1b4b3066b924ca880c59875d
AngeLSanchez210 <is21001@ues.edu.sv>
2023-11-23 15:38:55 -0600

    mostrarinfo

--- script/login.js
+++ script/login.js
@@ -1,35 +1,65 @@
-const btn = document.querySelector('[button]');
-btn.addEventListener('click', async function(e){
+const btn = document.getElementById('submitBtn');
+btn.addEventListener('click', async function (e) {
     e.preventDefault();
-    const user = document.querySelector('[username]');
-    const pass = document.querySelector('[password]');
+    const user = document.getElementById('username');
+    const pass = document.getElementById('password');
 
     const jss = {
         "username": user.value,
-        "email": "email@gm.com",
         "password": pass.value
     };
-    
-    fetch('http://127.0.0.1:8000/api/Login', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json; charset=utf-8"
-        },
-        body: JSON.stringify(jss)
-    }).then(async function(response) {
+
+    try {
+        const response = await fetch('http://127.0.0.1:8000/api/Login', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify(jss)
+        });
+
         if (!response.ok) {
-            return false;
+            console.error('Error al iniciar sesión:', response.status);
+            return;
         }
-        return response.json();
-    }).then(async function(data) {
 
-        if(data.res == true){
+        const data = await response.json();
+
+        if (data.res === true) {
             localStorage.setItem('token', JSON.stringify(data.token));
-            window.location.href = "../html/indexSession.html";
-        }
 
-    }).catch(function(err) {
-        console.error(err);
-    });
-    
+            const userDetailsResponse = await fetch(`http://127.0.0.1:8000/api/Usuario?username=${user.value}&password=${pass.value}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${data.token}`,
+                    'Content-Type': 'application/json; charset=utf-8',
+                },
+            });
+
+            if (!userDetailsResponse.ok) {
+                console.error('Error al obtener detalles del usuario:', userDetailsResponse.status);
+                return;
+            }
+
+            const userDetails = await userDetailsResponse.json();
+
+            const usuarioEncontrado = userDetails.find(u => u.username === user.value);
+
+            if (usuarioEncontrado) {
+                // Almacena los detalles del usuario en localStorage
+                localStorage.setItem('userInfo', JSON.stringify(usuarioEncontrado));
+
+                window.location.href = "../html/indexSession.html";
+            } else {
+                console.error('Usuario no encontrado.');
+            }
+        } else {
+            console.error('Credenciales incorrectas.');
+        }
+    } catch (error) {
+        console.error('Error en la solicitud de inicio de sesión:', error);
+    }
 });
+
+
+
