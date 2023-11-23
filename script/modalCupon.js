3b173e5a949ff7305253dbdb8506984bd472cde5
BeatrizzJimenezz <cj21004@ues.edu.sv>
2023-11-23 16:26:31 -0600

    a

--- script/modalCupon.js
+++ script/modalCupon.js
@@ -4,7 +4,7 @@
 function mostrarCupon(event) {
     event.preventDefault();
 
-    const libro = {
+    const cupon = {
         titulo: 'Canjea este cupón',
         porcentaje: '15%',
         imagen: '../img/logo22.png',
@@ -20,33 +20,78 @@ function mostrarCupon(event) {
         showConfirmButton: false,
         focusConfirm: false,
         heightAuto:false,
-        width:'55%',
+        width:'50%',
         position:'top',
 
         html: 
         `
         <article class="card2 fl-left">
-        <section class="date">
-          <time datetime="23th feb">
-            <span>23</span><span>feb</span>
-          </time>
-        </section>
-        <section class="card-cont">
-          <small>dj khaled</small>
-          <h3>corner obsest program</h3>
-          <div class="even-date">
-           <i class="fa fa-calendar"></i>
-           <time>
-             <span>wednesday 28 december 2014</span>
-             <span>08:55pm to 12:00 am</span>
-           </time>
-          </div>
-          <div class="even-info">
-            <i class="fa fa-map-marker"></i>
-          </div>
-          <a href="#">tickets</a>
-        </section>
-      </article>
+          <section class="date">
+            <time>
+              <span>${cupon.porcentaje}</span><span>Discount</span>
+            </time>
+          </section>
+          <section class="card-cont">
+            <small>Cupón</small>
+            <h3>Registrate y recibe un fabuloso descuento</h3>
+            <div class="even-date">
+            <i class="fa fa-calendar"></i>
+            <time>
+              <span>Expira: ${cupon.fecha}</span>
+            </time>
+            </div>
+            <div class="even-info">
+              <i class="fa fa-map-marker"></i>
+            </div>
+            <a href="../html/registro.html">Entrar</a>
+          </section>
+        </article>
         `,
     });
+}
+
+
+function mostrarCuponNavidad(event) {
+  event.preventDefault();
+
+  const cupon = {
+      titulo: 'Canjea este cupón',
+      porcentaje: '50%',
+      imagen: '../img/logo22.png',
+      descripcion: '¡En esta navidad, necesitas imaginación en tu vida, regala un libro!',
+      fecha: '31 de diciembre 2023'
+  };
+
+  
+
+  swal.fire({
+      showCloseButton: true,
+      showCancelButton: false,
+      showConfirmButton: false,
+      focusConfirm: false,
+      heightAuto:false,
+      width:'65%',
+      position:'top',
+      background: '#9D5C59',
+
+      html: 
+      `
+        <div class="fuente3 p-1 pt-0 pb-0">
+          <div class="row">
+
+            <div class="col-lg-6 col-xl-4 text-center" style="border-right-style:dashed; border-right-color: white;">
+                <img src="${cupon.imagen}" alt="${cupon.titulo}" style="width: 100%; max-height: 300px;">
+            </div>
+
+            <div class="col-lg-6 col-xl-8 text-white ps-4 text-center">
+                <h1 class="fuente1 text-center">${cupon.titulo}</h1>
+                <p class="text-white lh-0 mb-0 pb-0" style="font-size: 5rem;">${cupon.porcentaje} <small class="fs-6">off </small>  </p>
+                <p class="fs-4">${cupon.descripcion}</p>
+                <button class="btn btn-danger"">Recibir</button>
+            </div>
+
+          </div>
+       </div>
+      `,
+  });
 }
