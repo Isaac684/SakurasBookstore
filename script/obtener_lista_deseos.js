694d9ff4eb734701f696802b1d5403cf344a30b0
gnz-e <94329797+gnz-e@users.noreply.github.com>
2023-11-22 21:38:38 -0600

    cambios en guardar y eliminar la lista

---
+++ script/obtener_lista_deseos.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Recuperar la lista de deseos desde localStorage
+    let wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
+
+    // Obtener la tabla de deseos en el segundo HTML
+    let wishlistTable = document.getElementById('wishlist-table-body');
+
+    // Iterar sobre los libros en la lista de deseos y agregarlos a la tabla
+    wishlist.forEach(function(book) {
+        let row = wishlistTable.insertRow();
+        row.innerHTML = `
+            <td><img src="${book.imageUrl}" alt="" class="rounded-top-md align-self-center" width="60" height="95"></td>
+            <td><span class="titulo">${book.title}</span></td>
+            <td><span class="precio">${book.price}</span></td>
+            <td><button class="round-black-btn btn-dark" onclick="addToCart('${book.title}', '${book.price}')">Añadir al carrito</button></td>
+            <td><button class="round-black-btn btn-dark" onclick="removeFromWishlist('${book.title}')">Eliminar</button></td>
+        `;
+    });
+});
+
+// Función para eliminar un libro de la lista de deseos
+function removeFromWishlist(title) {
+    // Recuperar la lista de deseos desde localStorage
+    let wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
+
+    // Filtrar la lista de deseos para excluir el libro con el título proporcionado
+    wishlist = wishlist.filter(book => book.title !== title);
+
+    // Actualizar la lista de deseos en localStorage
+    localStorage.setItem('wishlist', JSON.stringify(wishlist));
+
+    // Recargar la página para reflejar los cambios en la interfaz de usuario
+    location.reload();
+}
