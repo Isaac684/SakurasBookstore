bf6bee90ec493e3a4182bca1629a30957d868f2b
gnz-e <94329797+gnz-e@users.noreply.github.com>
2023-11-24 08:39:38 -0600

    cositas

---
+++ script/paises.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener el elemento select
+    var selectPaises = document.getElementById("paises");
+
+    // Obtener la lista de países desde el enlace
+    fetch('http://127.0.0.1:8000/api/paises')
+      .then(response => response.json())
+      .then(data => {
+        // Iterar sobre el arreglo de países y agregar opciones al select
+        data.forEach(function(pais) {
+          var opcion = document.createElement("option");
+          opcion.value = pais.nombre;
+          opcion.text = pais.nombre;
+          selectPaises.add(opcion);
+        });
+      })
+      .catch(error => console.error('Error al obtener la lista de países:', error));
+    });
