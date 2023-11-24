1717224a4cd23b6be68c3e0d9b1c02e114f42423
BeatrizzJimenezz <cj21004@ues.edu.sv>
2023-11-23 21:59:00 -0600

    index administrador

--- script/productos.js
+++ script/productos.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', async function () {
     const searchInput = document.getElementById('searchInput');
-    const tableBody = document.querySelector('.table tbody');
+    const tableBody = document.getElementById('bodyProductos');
 
     try {
         const response = await fetch('http://127.0.0.1:8000/api/Productos', {
@@ -23,40 +23,28 @@ document.addEventListener('DOMContentLoaded', async function () {
         // Iterar sobre los productos y agregar filas a la tabla
         productos.forEach((producto, index) => {
             const row = tableBody.insertRow();
-            const cellNum = row.insertCell(0);
-            const cellNombre = row.insertCell(1);
-            const cellEditorial = row.insertCell(2);
-            const cellAutor = row.insertCell(3);
-            const cellCategoria = row.insertCell(4);
-            const cellImagen = row.insertCell(5);
-            const cellPrecio = row.insertCell(6);
-            const cellPrecioVenta = row.insertCell(7);
-            const cellAcciones = row.insertCell(8);
+            const cellNombre = row.insertCell(0);
+            const cellEditorial = row.insertCell(1);
+            const cellAutor = row.insertCell(2);
+            const cellAcciones = row.insertCell(3);
 
             // Rellenar las celdas con datos del producto
-            cellNum.textContent = index + 1;
             cellNombre.textContent = producto.name;
             cellEditorial.textContent = producto.editorial;
             cellAutor.textContent = producto.author;
-            cellCategoria.textContent = producto.category;
 
             const img = document.createElement('img');
             img.src = 'http://127.0.0.1:8000/api/files/' + producto.image; 
             img.style.width = '50px'; 
-            cellImagen.appendChild(img);
-
-            cellPrecio.textContent = '$' + producto.price;
-            cellPrecioVenta.textContent = '$' + producto.sell_price;
-
 
             // Botones de acciones (editar y eliminar)
             const btnEditar = document.createElement('button');
-            btnEditar.className = 'btn btn-outline-success btn-sm mx-1';
+            btnEditar.className = 'btn btn-outline-secondary p-1 me-1';
             btnEditar.innerHTML = '<svg class="bi" width="18" height="18" fill="currentColor"><use xlink:href="../style/bootstrap-icons-1.11.1/bootstrap-icons.svg#pencil"/></svg>';
             //btnEditar.addEventListener('click', () => editarProducto(producto));
 
             const btnEliminar = document.createElement('button');
-            btnEliminar.className = 'btn btn-outline-danger btn-sm mx-1';
+            btnEliminar.className = 'btn btn-outline-secondary p-1';
             btnEliminar.innerHTML = '<svg class="bi" width="18" height="18" fill="currentColor"><use xlink:href="../style/bootstrap-icons-1.11.1/bootstrap-icons.svg#trash"/></svg>';
             //btnEliminar.addEventListener('click', () => eliminarProducto(producto.id));
 
--- script/usuarios.js
+++ script/usuarios.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', async function () {
     const searchInput = document.getElementById('searchInput');
-    const tableBody = document.querySelector('.table tbody');
+    const tableBody = document.getElementById('bodyUsuarios');
 
     try {
         const response = await fetch('http://127.0.0.1:8000/api/Usuario', {
@@ -23,32 +23,24 @@ document.addEventListener('DOMContentLoaded', async function () {
         // Iterar sobre los usuarios y agregar filas a la tabla
         usuarios.forEach((usuario, index) => {
             const row = tableBody.insertRow();
-            const cellNum = row.insertCell(0);
-            const cellNombre = row.insertCell(1);
-            const cellEdad = row.insertCell(2);
-            const cellGenero = row.insertCell(3);
-            const cellEmail = row.insertCell(4);
-            const cellPassword = row.insertCell(5);
-            const cellPais = row.insertCell(6);
-            const cellAcciones = row.insertCell(7);
+            const cellNombre = row.insertCell(0);
+            const cellUsuario = row.insertCell(1);
+            const cellPais = row.insertCell(2);
+            const cellAcciones = row.insertCell(3);
 
             // Rellenar las celdas con datos del usuario
-            cellNum.textContent = index + 1;
             cellNombre.textContent = usuario.name;
-            cellEdad.textContent = usuario.age;
-            cellGenero.textContent = usuario.gender;
-            cellEmail.textContent = usuario.email;
-            cellPassword.textContent = usuario.password;
+            cellUsuario.textContent = usuario.username;
             cellPais.textContent = usuario.country;
 
             // Botones de acciones (editar y eliminar)
             const btnEditar = document.createElement('button');
-            btnEditar.className = 'btn btn-outline-success btn-sm mx-1';
+            btnEditar.className = 'btn btn-outline-secondary btn-sm mx-1';
             btnEditar.innerHTML = '<svg class="bi" width="18" height="18" fill="currentColor"><use xlink:href="../style/bootstrap-icons-1.11.1/bootstrap-icons.svg#pencil"/></svg>';
             //btnEditar.addEventListener('click', () => editarUsuario(usuario));
 
             const btnEliminar = document.createElement('button');
-            btnEliminar.className = 'btn btn-outline-danger btn-sm mx-1';
+            btnEliminar.className = 'btn btn-outline-secondary btn-sm mx-1';
             btnEliminar.innerHTML = '<svg class="bi" width="18" height="18" fill="currentColor"><use xlink:href="../style/bootstrap-icons-1.11.1/bootstrap-icons.svg#trash"/></svg>';
             //btnEliminar.addEventListener('click', () => eliminarUsuario(usuario.id));
 
