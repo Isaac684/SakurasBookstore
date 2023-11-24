49db8a5fda644bef3da9798a9d6959980a383943
gnz-e <94329797+gnz-e@users.noreply.github.com>
2023-11-23 23:43:35 -0600

    eliminacion 1/2

--- script/productos.js
+++ script/productos.js
@@ -1,62 +1,3 @@
-// document.addEventListener('DOMContentLoaded', async function () {
-//     const searchInput = document.getElementById('searchInput');
-//     const tableBody = document.getElementById('bodyProductos');
-
-//     try {
-//         const response = await fetch('http://127.0.0.1:8000/api/Productos', {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json; charset=utf-8',
-//             },
-//         });
-
-//         if (!response.ok) {
-//             console.error('Error al obtener la lista de productos:', response.status);
-//             return;
-//         }
-
-//         const productos = await response.json();
-
-//         // Limpiar el cuerpo de la tabla
-//         tableBody.innerHTML = '';
-
-//         // Iterar sobre los productos y agregar filas a la tabla
-//         productos.forEach((producto, index) => {
-//             const row = tableBody.insertRow();
-//             const cellNombre = row.insertCell(0);
-//             const cellEditorial = row.insertCell(1);
-//             const cellAutor = row.insertCell(2);
-//             const cellAcciones = row.insertCell(3);
-
-//             // Rellenar las celdas con datos del producto
-//             cellNombre.textContent = producto.name;
-//             cellEditorial.textContent = producto.editorial;
-//             cellAutor.textContent = producto.author;
-
-//             const img = document.createElement('img');
-//             img.src = 'http://127.0.0.1:8000/api/files/' + producto.image; 
-//             img.style.width = '50px'; 
-
-//             // Botones de acciones (editar y eliminar)
-//             const btnEditar = document.createElement('button');
-//             btnEditar.className = 'btn btn-outline-secondary p-1 me-1';
-//             btnEditar.innerHTML = '<svg class="bi" width="18" height="18" fill="currentColor"><use xlink:href="../style/bootstrap-icons-1.11.1/bootstrap-icons.svg#pencil"/></svg>';
-//             //btnEditar.addEventListener('click', () => editarProducto(producto));
-
-//             const btnEliminar = document.createElement('button');
-//             btnEliminar.className = 'btn btn-outline-secondary p-1';
-//             btnEliminar.innerHTML = '<svg class="bi" width="18" height="18" fill="currentColor"><use xlink:href="../style/bootstrap-icons-1.11.1/bootstrap-icons.svg#trash"/></svg>';
-//             //btnEliminar.addEventListener('click', () => eliminarProducto(producto.id));
-
-//             cellAcciones.appendChild(btnEditar);
-//             cellAcciones.appendChild(btnEliminar);
-//         });
-
-//     } catch (error) {
-//         console.error('Error en la solicitud de obtener productos:', error);
-//     }
-// });
-
 document.addEventListener('DOMContentLoaded', async function () {
     const searchInput = document.getElementById('searchInput1');
     const tableBody = document.getElementById('bodyProductos');
@@ -106,8 +47,8 @@ document.addEventListener('DOMContentLoaded', async function () {
                 const btnEliminar = document.createElement('button');
                 btnEliminar.className = 'btn btn-outline-secondary p-1';
                 btnEliminar.innerHTML = '<svg class="bi" width="18" height="18" fill="currentColor"><use xlink:href="../style/bootstrap-icons-1.11.1/bootstrap-icons.svg#trash"/></svg>';
-                //btnEliminar.addEventListener('click', () => eliminarProducto(producto.id));
-
+                btnEliminar.addEventListener('click', () => eliminarProducto(producto.id));
+                
                 cellAcciones.appendChild(btnEditar);
                 cellAcciones.appendChild(btnEliminar);
             });
@@ -125,6 +66,59 @@ document.addEventListener('DOMContentLoaded', async function () {
             tableBody.innerHTML = ''; // Limpiar la tabla antes de agregar las filas filtradas
             agregarFilas(productosFiltrados);
         }
+
+        async function eliminarProducto(id) {
+
+            const result = await Swal.fire({
+                title: '¿Estás seguro?',
+                text: '¡No podrás revertir esto!',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#3085d6',
+                confirmButtonText: 'Sí, eliminarlo',
+                showLoaderOnConfirm: true,
+                customClass: {
+                    content: 'text-center' 
+                }
+            });
+
+            if (result.isConfirmed) {
+                try {
+                    // Enviar la solicitud de eliminación solo si el usuario confirma
+                    const response = await fetch(`http://127.0.0.1:8000/api/Productos/${id}`, {
+                        method: 'DELETE',
+                        headers: {
+                            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('token'))
+                        },
+                    });
+
+                    if (!response.ok) {
+                        console.error('Error al eliminar el producto:', response.status);
+                        return;
+                    }
+
+                    // Actualizar la lista de productos después de la eliminación
+                    const updatedProductos = originalProductos.filter(producto => producto.id !== id);
+                    originalProductos = updatedProductos;
+
+                    // Volver a mostrar la tabla actualizada
+                    filtrarTabla();
+
+                    // Mostrar mensaje de éxito con SweetAlert
+                    Swal.fire(
+                        '¡Eliminado!',
+                        'El producto ha sido eliminado.',
+                        'success'
+                    );
+
+                    console.log('Producto eliminado exitosamente!');
+                } catch (error) {
+                    console.error('Error al eliminar el producto:', error);
+                }
+            }
+        }
+
 
         // Mostrar todos los productos inicialmente
         agregarFilas(originalProductos);
