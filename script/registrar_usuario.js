0e71fef7902cefa67f31b4e5deae15a246f2409a
BeatrizzJimenezz <cj21004@ues.edu.sv>
2023-11-25 00:55:41 -0600

    validaciones registro

--- script/registrar_usuario.js
+++ script/registrar_usuario.js
@@ -33,62 +33,86 @@ submitButton.addEventListener('click', async function(e){
     const direccionPrincipal = document.querySelector('[d-principal]');
     const direccionesEnvio = document.querySelector('[d-envios]');
 
-    console.log(genero);
-
 
     if (foto.files.length > 0) {
         const file = foto.files[0];
         const reader = new FileReader();reader.onload = function (e) {
+
+            const formData = new FormData();
+
+            var passwordRegex = /^(?=.*[+*.])(?!.*\s).{8,}$/;
+
+            if (passwordRegex.test(contrasena.value)) {
+                formData.append('password', contrasena.value);
+                formData.append('username', usuario.value);
+                formData.append('email', correo.value);
+                formData.append('password', contrasena.value);
+                formData.append('name', nombre.value);
+                formData.append('age', edad.value);
+                formData.append('gender', genero.value);
+                formData.append('file', file);
+                formData.append('country', paises.value);
+                formData.append('address', direccionPrincipal.value);
+                formData.append('send_address', direccionesEnvio.value);
+                formData.append('refer_code', '64ZTR2FNPQ'); 
+
+                passwordError.textContent = "";
+    
+                fetch('http://127.0.0.1:8000/api/Usuario', {
+                    method: 'POST',
+                    headers: {
+                        
+                    },
+                    body: formData,
+                }).then(async function(response) {
+                    if (!response.ok) {
+                        return false;
+                    }
+                    return response.json();
+                }).then(function(data) {
+                    if(data.res == true){
+                        Swal.fire({
+                            title: 'Registrado',
+                            text: 'Has sido registrado con exito.',
+                            icon: 'success',
+                            confirmButtonText: 'Aceptar'
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                window.location.href = 'http://127.0.0.1:5501/html/indexSession.html';
+                            }
+                        });
+                    }
+                    else
+                    {
+                        Swal.fire({
+                            title: 'Opps..',
+                            text: '¡Este usuario o email ya existen! Cambia tus datos.',
+                            icon: 'error',
+                            confirmButtonText: 'Aceptar'
+                        });
+                    }
+    
+                }).catch(function(err) {
+                    console.error(err);
+                });
+            } else {
+                passwordError.textContent = "La contraseña no cumple con los requisitos.";
+            }
        
-        const formData = new FormData();
-        formData.append('username', usuario.value);
-        formData.append('email', correo.value);
-        formData.append('password', contrasena.value);
-        formData.append('name', nombre.value);
-        formData.append('age', edad.value);
-        formData.append('gender', genero.value);
-        console.log(genero.value);
-        formData.append('file', file);
-        formData.append('country', paises.value);
-        formData.append('address', direccionPrincipal.value);
-        formData.append('send_address', direccionesEnvio.value);
-        formData.append('refer_code', '64ZTR2FNPQ'); 
-
-            fetch('http://127.0.0.1:8000/api/Usuario', {
-                method: 'POST',
-                headers: {
-                    
-                },
-                body: formData,
-            }).then(async function(response) {
-                if (!response.ok) {
-                    return false;
-                }
-                
-                return response.json();
-            }).then(function(data) {
-                console.log("en teoria se mandaron los datos");
-                if(data.res == true){
-                    Swal.fire({
-                        title: 'Registrado',
-                        text: 'Has sido registrado con exito.',
-                        icon: 'success',
-                        confirmButtonText: 'Aceptar'
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            // Redirige a la página deseada
-                        window.location.href = 'http://127.0.0.1:5501/html/indexSession.html';
-                        }
-                    });
-                }
-
-            }).catch(function(err) {
-                console.error(err);
-            });
         };
 
         reader.readAsDataURL(file);
     }
+    else {
+        Swal.fire({
+            title: 'Opps..',
+            text: '¡Tu foto es demasiado grande! Cambia por una mas pequeña.',
+            icon: 'error',
+            confirmButtonText: 'Aceptar'
+        });
+    }
+
+
 
 
 
